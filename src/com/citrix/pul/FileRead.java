1ba6d280e1a12d055fca4dfb8e5aa1c252e4f4f6
PaulLiu9527 <pul@NKGWPUL01.citrite.net>
2019-05-16 17:59:02 +0800

    5/16/2019 In Office,Echo

--- src/com/citrix/pul/FileRead.java
+++ src/com/citrix/pul/FileRead.java
@@ -1,34 +1,78 @@
 package com.citrix.pul;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
+import java.nio.channels.SocketChannel;
+
+import org.junit.Before;
+import org.junit.Test;
 
 public class FileRead {
 
-	public static void main(String[] args) throws Exception {
-		RandomAccessFile aFile = new RandomAccessFile("D:\\eclipse\\WORKSPACE\\NIO_LEARNING\\src\\nio-data.txt", "rw");
-		FileChannel inChannel = aFile.getChannel();
+	public RandomAccessFile aFile;
+	public FileChannel inChannel;
+	public ByteBuffer buf;
 
-		ByteBuffer buf = ByteBuffer.allocate(48);
+	@Before
+	public void beforeTest() throws IOException {
+		aFile = new RandomAccessFile("src\\com\\citrix\\pul\\nio-data.txt", "rw");
+		inChannel = aFile.getChannel();
+		buf = ByteBuffer.allocate(48);
+	}
+
+	@Test
+	public void useBufferToRead() throws Exception {
+		/*
+		 * Remind: if you forget the different between absolute-path and relative-path,
+		 * try this: File file =new File("/test.txt") //F:\test.txt File file =new
+		 * File("test.txt") //F:\gitProject\NIO_LEARNING\test.txt
+		 * system.out.println(file.getAbsolutePath());
+		 */
 
 		int bytesRead = inChannel.read(buf);
 		while (bytesRead != -1) {
-
 			System.out.println("Read " + bytesRead);
-
 			buf.flip();
-
 			while (buf.hasRemaining()) {
 				System.out.print((char) buf.get());
 			}
-
 			buf.clear();
 			bytesRead = inChannel.read(buf);
 		}
 		aFile.close();
-
 	}
 
+	@Test
+	public void useBufferMoreToRead() throws Exception {
+		inChannel.read(buf);
+		buf.flip();
+		System.out.println((char) buf.get());
+		System.out.println(buf.position());
+		System.out.println("....................");
+		buf.rewind();
+		System.out.println(buf.position());
+		System.out.println((char)buf.get());
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
